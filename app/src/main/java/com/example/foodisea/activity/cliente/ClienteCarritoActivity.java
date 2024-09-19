a9529940fa1fa315642e593f80084eb4478b2127
Allison <a20196483@pucp.edu.pe>
2024-09-19 13:00:56 -0500

    Vista cliente - carrito

--- app/src/main/java/com/example/foodisea/activity/cliente/ClienteCarritoActivity.java
+++ app/src/main/java/com/example/foodisea/activity/cliente/ClienteCarritoActivity.java
@@ -9,8 +9,21 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.foodisea.R;
+import com.example.foodisea.adapter.CarritoAdapter;
+import com.example.foodisea.entity.ProductoCarrito;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClienteCarritoActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private CarritoAdapter adapter;
+    private List<ProductoCarrito> productoCarritos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +35,21 @@ public class ClienteCarritoActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        // Inicializar el RecyclerView
+        recyclerView = findViewById(R.id.rvCartItems);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Inicializar la lista de productos del carrito
+        productoCarritos = new ArrayList<>();
+
+        // Agregar ítems de prueba al carrito
+        productoCarritos.add(new ProductoCarrito("Hamburguesa",64.00, R.drawable.burger, 2));
+        productoCarritos.add(new ProductoCarrito("Hamburguesa VB", 32.00, R.drawable.burger, 1));
+
+        // Inicializar el adaptador y asignarlo al RecyclerView
+        adapter = new CarritoAdapter(productoCarritos);
+        recyclerView.setAdapter(adapter);
     }
 }
