31d64a426a450771cb2316e80bcc6eeae2610d58
Diego Henriquez <a20182044@pucp.edu.pe>
2024-09-19 20:10:41 -0500

    Add:vistas admin de restaurante

---
+++ app/src/main/java/com/example/foodisea/activity/adminRes/HomeAdminResActivity.java
@@ -0,0 +1,26 @@
+package com.example.foodisea.activity.adminRes;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.foodisea.R;
+
+public class HomeAdminResActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_home_admin_res);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
