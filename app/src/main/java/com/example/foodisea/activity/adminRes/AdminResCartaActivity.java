b8532eac016e2869a5cacb8163bfd996fb9cf7de
Diego Henriquez <a20182044@pucp.edu.pe>
2024-09-23 15:08:07 -0500

    Change:nombre de archivos para admin de res

--- app/src/main/java/com/example/foodisea/activity/adminRes/AdminResCartaActivity.java
+++ app/src/main/java/com/example/foodisea/activity/adminRes/AdminResCartaActivity.java
@@ -10,13 +10,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.foodisea.R;
 
-public class HomeAdminResActivity extends AppCompatActivity {
+public class AdminResCartaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_home_admin_res);
+        setContentView(R.layout.activity_admin_res_carta);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
