4c1a5234f4edb100c6357dcd9a98de57f862da6f
eds0c <eduardo.carbajal@pucp.edu.pe>
2024-09-28 21:50:05 -0500

    algunas vistas más de superadmin

---
+++ app/src/main/java/com/example/foodisea/activity/superadmin/SuperAdminGestionUsuariosActivity.java
@@ -0,0 +1,26 @@
+package com.example.foodisea.activity.superadmin;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.foodisea.R;
+
+public class SuperAdminGestionUsuariosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_super_admin_gestion_usuarios);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
