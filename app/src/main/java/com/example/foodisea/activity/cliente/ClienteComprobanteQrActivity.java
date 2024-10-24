33f0a8f1830d4278b805111347a4e0744e150a88
Diego Henriquez <a20182044@pucp.edu.pe>
2024-10-24 13:37:42 -0500

    Add: notificaciones cuando se confirma la entrega del pedido

--- app/src/main/java/com/example/foodisea/activity/cliente/ClienteComprobanteQrActivity.java
+++ app/src/main/java/com/example/foodisea/activity/cliente/ClienteComprobanteQrActivity.java
@@ -1,15 +1,27 @@
 package com.example.foodisea.activity.cliente;
 
+import static android.Manifest.permission.POST_NOTIFICATIONS;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.foodisea.R;
+import com.example.foodisea.activity.adminRes.AdminResHomeActivity;
+import com.example.foodisea.activity.adminRes.AdminResPedidosActivity;
 import com.example.foodisea.databinding.ActivityClienteComprobanteQrBinding;
 
 public class ClienteComprobanteQrActivity extends AppCompatActivity {
@@ -35,5 +47,49 @@ public class ClienteComprobanteQrActivity extends AppCompatActivity {
             finish(); // Cierra la actividad actual y regresa
         });
 
+        createNotificationChannel();
+
+        binding.btnQrPago.setOnClickListener(view -> {
+            lanzarNotificacion();
+        });
+
+    }
+
+    String channelId = "channelDefaultPri";
+    public void createNotificationChannel(){
+        if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel(channelId, "Canal notificaciones default", NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription("Canal para notificaciones con prioridad default");
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+
+            askPermission();
+        }
+    }
+
+    public void askPermission(){
+        if(android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU && ActivityCompat.checkSelfPermission(this, POST_NOTIFICATIONS) == PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(ClienteComprobanteQrActivity.this,new String[]{POST_NOTIFICATIONS},101);
+        }
+    }
+
+    public void lanzarNotificacion(){
+        Intent intent = new Intent(this, AdminResPedidosActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_IMMUTABLE);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channelId)
+                .setSmallIcon(R.drawable.ic_notifications)
+                .setContentTitle("Tu pedido ha sido completado")
+                .setContentText("Tocar para ver más detalles")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        if(ActivityCompat.checkSelfPermission(this,POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED){
+            notificationManager.notify(1,builder.build());
+        }
     }
 }
